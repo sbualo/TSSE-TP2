30ccbdc85326955272c6576e7c734dc2153d5abd
sbualo <sbualo@itba.edu.ar>
2024-03-21 19:25:35 -0300

    Solve issue #2 and #3: Se comenta el documento leds.h y se eliminan las macros públicas

--- leds/inc/leds.h
+++ leds/inc/leds.h
@@ -1,12 +1,26 @@
 
 #include <stdint.h>
-#define LED_TO_BIT(x) (1 << (x - 1))
-#define ALL_LED_OFF   0x00
+#include <stdbool.h>
 
-#define ALL_LED_ON    0b1111111111111111
+// Documentar como si fuese un driver
 
+/**
+ * @brief Funcion que inicializa los LEDs y setea a los LEDs en OFF
+ *
+ * @param puerto Direccion en la que se encuentran los LEDs
+ */
 void leds_init(uint16_t * puerto);
 
+/**
+ * @brief Funcion que prende un led especificado
+ *
+ * @param led numero de led a prender
+ */
 void leds_turn_on(int led);
 
+/**
+ * @brief funcion que apaga un led especificado
+ *
+ * @param led Numero de led a apagar
+ */
 void leds_turn_off(int led);
